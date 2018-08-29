3b730f6dd48dcb05d55deb983fbae00641f53181
tomasJ8 <42393935+tomasJ8@users.noreply.github.com>
2018-08-29 18:50:06 -0500

    Add files via upload

---
+++ Cursos_Bootstrap/Json_lista.js
@@ -0,0 +1,102 @@
+alumnos = [
+    {
+        "nombre" : "Diego",
+        "apellido" : "Cueto",
+        "edad" : 23,
+        "sexo" : "H",
+        "gmail" : "diegocueto-9@hotmail.com",
+        "telefono" : 966232309,
+        "direccioncalle" : "16 de Septiembre",
+        "colonia" : "Nicatan",
+        "municipio" : "Tonala",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Lucio",
+        "apellido" : "Escobar",
+        "edad" : 22,
+        "sexo" : "H",
+        "gmail" : "lucio48@outlook.com",
+        "telefono" : 9681197536,
+        "direccioncalle" : "Revolucion",
+        "colonia" : "Union Agrarista",
+        "municipio" : "Jiquipilas",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Tomas",
+        "apellido" : "Lopez",
+        "edad" : 22,
+        "sexo" : "H",
+        "gmail" : "thomas_mcthom@Outlook.com",
+        "telefono" : 9681009079,
+        "direccioncalle" : "Segunda Avenida Sur",
+        "colonia" : "Santa Cruz",
+        "municipio" : "Cintalapa",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    {
+        "nombre" : "Fidel",
+        "apellido" : "Rodriguez",
+        "edad" : 22,
+        "sexo" : "H",
+        "gmail" : "fidelin24_@hotmail.com",
+        "telefono" : 9681143111,
+        "direccioncalle" : "Avenida calle central",
+        "colonia" : "Las Merceditas",
+        "municipio" : "Cintalapa",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+{
+    "nombre":"Lorena",
+    "apellido":"Zarate",
+    "edad":22,
+    "email":"anerol_brumars@hotmail.com",
+    "sexo":"M"
+    "Direccion",
+       "calle":"8a oriente entre 4a y 5a Norte",
+       "No_Ex":454,
+       "colonia":"San Francisco",
+       "municipio": "Cintalapa",
+       "estado":"Chiapas",
+       "pais":"Mexico"
+  },
+  {
+    "nombre":"Roxana",
+    "apellido":"Salazar",
+    "edad":22,
+    "email":"monce.26@hotmail.com",
+    "sexo":"M"
+    "Direccion",
+       "calle":"Cuadra y media de la entrada de tehuacan",
+       "No_Ex":5,
+       "colonia":"Tehuacan",
+       "municipio": "Cintalapa",
+       "estado":"Chiapas",
+       "pais":"Mexico"
+  },
+  {
+    "nombre":"Guadalupe",
+    "apellido":"Aguilar",
+    "edad":22,
+    "email":"guadalupeaguilarmedina@oulook.com",
+    "sexo":"M"
+    "Direccion",
+       "calle":"",
+       "No_Ex":,
+       "colonia":"",
+       "municipio": "San Isidro",
+       "estado":"Chiapas",
+       "pais":"Mexico"
+  },
+    
